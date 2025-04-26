115ac74c724da8f539a428cf434d675d11b46eda
Terragul-Mason <artem.galenin@yandex.ru>
2025-04-26 23:13:25 +0300

    Написал класс для сравнения ArrayList и LinkedList

---
+++ src/main/java/ListPerformanceTest/ListPerformanceTest.java
@@ -0,0 +1,38 @@
+package ListPerformanceTest;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListPerformanceTest {
+
+    public static void main(String[] args) {
+        int operationsCount = 1000;
+
+        System.out.println("ArrayList:");
+        testListPerformance(new ArrayList<>(), operationsCount);
+
+        System.out.println("LinkedList:");
+        testListPerformance(new LinkedList<>(), operationsCount);
+    }
+
+    public static void testListPerformance(List<Integer> list, int operationsCount) {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < operationsCount; i++)
+            list.add(i);
+        long endTime = System.nanoTime();
+        System.out.println("add(" + operationsCount + "): " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        for (int i = 0; i < operationsCount; i++)
+            list.get(i);
+        endTime = System.nanoTime();
+        System.out.println("get(" + operationsCount + "): " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        for (int i = 0; i < operationsCount; i++)
+            list.remove(0);
+        endTime = System.nanoTime();
+        System.out.println("remove(" + operationsCount + "): " + (endTime - startTime) + " ns");
+    }
+}
